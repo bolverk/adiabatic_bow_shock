93f36a3693a3bb701e3c4d975e1d8d769428db10
Almog Yalinewich <almog.yalin@gmail.com>
2015-07-22 14:26:44 +0300

    extract initial conditions from main file

--- rich.cpp
+++ rich.cpp
@@ -37,29 +37,12 @@
 #include "source/newtonian/test_2d/multiple_diagnostics.hpp"
 #include "source/newtonian/two_dimensional/source_terms/cylindrical_complementary.hpp"
 #include "source/misc/vector_initialiser.hpp"
+#include "calc_init_cond.hpp"
 
 using namespace std;
 using namespace simulation2d;
 
 namespace {
-
-  vector<ComputationalCell> calc_init_cond(const Tessellation& tess)
-  {
-    vector<ComputationalCell> res(static_cast<size_t>(tess.GetPointNo()));
-    const Circle obstacle(Vector2D(0,0),0.01);
-    for(size_t i=0;i<res.size();++i){
-      const Vector2D r = tess.GetMeshPoint(static_cast<int>(i));
-      res[i].density = 1;
-      res[i].pressure = 1e-9;
-      res[i].velocity = Vector2D(0,1);
-      res[i].stickers["obstacle"] = false;
-      if(obstacle(r)){
-	res[i].velocity = Vector2D(0,0);
-	res[i].stickers["obstacle"] = true;
-      }
-    }
-    return res;
-  }
 
   class CustomCellUpdater: public CellUpdater
   {
