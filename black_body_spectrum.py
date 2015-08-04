2082978e523be2fd3ebed0b016dbd9192542c25d
Almog Yalinewich <almog.yalin@gmail.com>
2015-08-04 15:17:31 +0300

    create script to calculate spectrum from simulation

--- black_body_spectrum.py
+++ black_body_spectrum.py
@@ -1,3 +1,15 @@
+def cache_eval(func, fname):
+
+    import os
+    import pickle
+
+    if os.path.isfile(fname):
+        return pickle.load(open(fname,'rb'))
+    else:
+        res = func()
+        pickle.dump(res,open(fname,'wb'))
+        return res
+
 def spectrum_integrate(area_list, temperature_list, frequency_list, constants):
 
     import numpy
@@ -74,6 +86,60 @@ def snr_knot():
     pylab.ylabel(r'$F_\nu$ [erg/s/Hz]')
     pylab.show()
 
+class RightRectrangle:
+
+    def __init__(self,x_min, x_max, y_min, y_max):
+
+        self.x_min = x_min
+        self.y_min = y_min
+        self.x_max = x_max
+        self.y_max = y_max
+
+    def __call__(self, x, y):
+
+        return (self.x_max > x > self.x_min) and (self.y_max > y > self.y_min)
+
+def calc_max_temperature(g=None,m=None,v=None,k=None):
+        
+    return 2*(g-1)*m*v**2/k/(g+1)**2
+
+def real_deal():
+
+    import pickle
+    import numpy
+    import pylab
+
+    t_front = pickle.load(open('bb_t_front.pkl','rb'))
+    a_front = pickle.load(open('bb_a_front.pkl','rb'))
+
+    constants = {'speed of light':3e10,
+                 'planck constant':6.626e-27,
+                 'boltzmann constant':1.38e-16,
+                 'solar radius':6.96e10,
+                 'bremsstrahlung prefactor':6.8e-38,
+                 'kelvin':1,
+                 'electron volt':1.6e-12,
+                 'wind velocity':1000*1000*1e2,
+                 'adiabatic index':5./3.,
+                 'proton mass':1.67e-24,
+                 'electron mass':9.1e-28}
+    max_temperatue = calc_max_temperature(g = constants['adiabatic index'],
+                                              m = constants['proton mass'],
+                                              v = constants['wind velocity'],
+                                              k = constants['boltzmann constant'])
+    scaled = {}
+    scaled['areas'] = numpy.array(a_front)*constants['solar radius']**2
+    scaled['temperature'] = numpy.array(t_front)*max_temperatue/numpy.max(t_front)
+    frequency_list = numpy.logspace(11,20,num=100)
+    spectrum = spectrum_integrate(scaled['areas'],
+                                  scaled['temperature'],
+                                  frequency_list,
+                                  constants)
+    pylab.loglog(frequency_list*constants['planck constant']/constants['electron volt'],
+                 spectrum)
+    pylab.show()
+        
 if __name__ == '__main__':
 
-    snr_knot()
+    #snr_knot()
+    real_deal()
