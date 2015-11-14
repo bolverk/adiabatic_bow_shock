89e171209504c0dea1e269bc518f0040321226ac
Almog Yalinewich <almog.yalin@gmail.com>
2015-11-14 04:04:18 +0200

    adapt diagnostics for parallel run

--- my_main_loop.cpp
+++ my_main_loop.cpp
@@ -6,20 +6,23 @@
 #include "volume_appendix.hpp"
 #include "temperature_appendix.hpp"
 #include "write_wall_time.hpp"
+#include <boost/lexical_cast.hpp>
 
 using namespace simulation2d;
 
 void my_main_loop(hdsim& sim)
 {
+  const boost::mpi::communicator world;
+  const string rank_s = boost::lexical_cast<string>(world.rank());
   const double tf = 10;
   SafeTimeTermination term_cond(tf,1e6);
   MultipleDiagnostics diag
     (VectorInitialiser<DiagnosticFunction*>
-     (new WriteTime("time.txt"))
-     (new WriteWallTime("wall_time.txt"))
+     (new WriteTime("time_"+rank_s+".txt"))
+     (new WriteWallTime("wall_time"+rank_s+".txt"))
      (new ConsecutiveSnapshots
       (new ConstantTimeInterval(tf/1000),
-       new Rubric("snapshot_",".h5"),
+       new Rubric("snapshot_"+rank_s+"_",".h5"),
        VectorInitialiser<DiagnosticAppendix*>
        (new TemperatureAppendix(1,1))
        (new VolumeAppendix())()
@@ -28,5 +31,5 @@ void my_main_loop(hdsim& sim)
 	    term_cond,
 	    &hdsim::TimeAdvance,
 	    &diag);
-  write_snapshot_to_hdf5(sim, "final.h5");
+  write_snapshot_to_hdf5(sim, "final_"+rank_s+".h5");
 }
