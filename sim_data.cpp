0bc61477fa4099cf072e329c440d48e843f05132
Almog Yalinewich <almog.yalin@gmail.com>
2015-12-29 09:29:14 +0200

    tweak parameters

--- sim_data.cpp
+++ sim_data.cpp
@@ -4,8 +4,8 @@
 SimData::SimData(void):
   pg_(Vector2D(0,0), Vector2D(0,1)),
   outer_(Vector2D(0,-0.5),
-	 Vector2D(1.0,0.5)),
-  init_points_(cartesian_mesh(100,100,
+	 Vector2D(2.0,4.5)),
+  init_points_(cartesian_mesh(200,500,
 			      outer_.getBoundary().first,
 			      outer_.getBoundary().second)),
   tess_(init_points_, outer_),
@@ -15,7 +15,7 @@ SimData::SimData(void):
   rs_(),
   geom_source_(pg_.getAxis()),
   wind_source_
-  (4*M_PI/1e3/(4.0*M_PI*pow(0.01,3)/3.),
+  (4*M_PI/1e4/(4.0*M_PI*pow(0.01,3)/3.),
    10,
    1e-3,
    0.01),
